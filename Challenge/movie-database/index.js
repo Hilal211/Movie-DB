d81fb0aafd86a0f4eba67ba0f33d5112f3eabad3
hilal masri <hilal1922001@gmail.com>
2021-03-30 01:17:06 +0300

    step 3

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const hours=new Date().getHours();
+const minutes=new Date().getMinutes();
+const TIME=hours+":"+minutes;
 
 app.get('/', (req, res) => {
   res.send('ok')
@@ -9,3 +12,11 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+app.get('/test', function (req, res) {
+    res.send({status:200, message:"ok"})
+  })
+
+  app.get('/time', function (req, res) {
+    res.send({status:200, message:TIME})
+  })
