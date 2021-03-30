83baa2bca3f9304f8ffcacca1e5a3bf2b30b818d
hilal masri <hilal1922001@gmail.com>
2021-03-30 21:39:08 +0300

    step 4

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -20,3 +20,17 @@ app.get('/test', function (req, res) {
   app.get('/time', function (req, res) {
     res.status(200).send(TIME)
   })
+
+  app.get('/Hello', function (req, res) {
+    res.status(200).send("Hello")
+  })
+
+  app.get('/Hello/:id', function (req, res) {
+    res.status(200).send("Hello " + req.params.id)
+  })
+
+  app.get('/search?s', function (req, res) {
+    if(req.query.s == "" || req.query.s == undefined){res.status(500).send('You have to enter a search')}
+    else{res.status(200).send("OK! You searched for:" + req.query.s)}
+})
+  
