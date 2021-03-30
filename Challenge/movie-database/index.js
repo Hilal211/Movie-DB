26e049ab66287696313cb501c2f34b572c3fef99
hilal masri <hilal1922001@gmail.com>
2021-03-30 22:00:00 +0300

    step 5

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -4,6 +4,12 @@ const port = 3000
 const hours=new Date().getHours();
 const minutes=new Date().getMinutes();
 const TIME=hours+":"+minutes;
+const movies = [
+  { title: 'Jaws', year: 1975, rating: 8 },
+  { title: 'Avatar', year: 2009, rating: 7.8 },
+  { title: 'Brazil', year: 1985, rating: 8 },
+  { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
 
 app.get('/', (req, res) => {
   res.send('ok')
@@ -29,8 +35,24 @@ app.get('/test', function (req, res) {
     res.status(200).send("Hello " + req.params.id)
   })
 
-  app.get('/search?s', function (req, res) {
+  app.get('/search?s=:search', function (req, res) {
     if(req.query.s == "" || req.query.s == undefined){res.status(500).send('You have to enter a search')}
     else{res.status(200).send("OK! You searched for:" + req.query.s)}
 })
+
+app.get('/movies/create', (req, res) => {
+  res.send("Create")
+});
+
+app.get('/movies/read', (req, res) => {
+  res.status(200).send(movies)
+});
+
+app.get('/movies/update', (req, res) => {
+  res.send("Update")
+});
+
+app.get('/movies/delete', (req, res) => {
+  res.send("Delete")
+});
   
